413171f3947b58d4d187ebcb6ee085299b7b651d
raffaele <raffaele.fontana.02@gmail.com>
2024-03-09 00:07:23 +0100

    create index for assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import blackLogo from './blackLogo.png'
+import whiteLogo from './whiteLogo.png'
+import tabLogo from './tabLogo.png'
+import hamburger from './hamburger.svg'
+
+export{
+    blackLogo,
+    whiteLogo,
+    tabLogo,
+    hamburger
+}
