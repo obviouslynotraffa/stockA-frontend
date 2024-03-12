c50bce207a142b509d7647ffa022c4e04b8dfb0e
raffaele <raffaele.fontana.02@gmail.com>
2024-03-13 00:22:07 +0100

    add brand images

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,30 @@
-
+import { Nike } from '../assets/images/index'
+import {Jordan} from '../assets/images/index'
+import {Yeezy} from '../assets/images/index'
+import {OffWhite} from '../assets/images/index'
 
 export const navLinks = [
     { href: "/", label: "Home" },
     { href: "/products", label: "Products" },
     { href: "/about", label: "About Us" },
     { href: "/contact", label: "Contact" },
+];
+
+export const BrandImages = [
+    {
+        src: Nike, 
+        alt: "nike logo",
+    },
+    {
+        src: Jordan, 
+        alt: "jordan logo",
+    },
+    {
+        src: OffWhite,
+         alt: "off-white logo",
+    },
+    {
+        src: Yeezy,
+         alt: "yeezy logo",
+    },
 ];
