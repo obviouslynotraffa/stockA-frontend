07ceee95f7455834b53c84dbce0d78d1a0a60138
raffaele <raffaele.fontana.02@gmail.com>
2024-03-13 00:22:14 +0100

    update brand section

--- src/components/Home/BestBrands.jsx
+++ src/components/Home/BestBrands.jsx
@@ -1,7 +1,5 @@
-import { Nike } from "../../assets/images"
-import { Jordan } from "../../assets/images"
-import { OffWhite } from "../../assets/images"
-import { Yeezy } from "../../assets/images"
+import { BrandImages } from "../../constants"
+
 
 const BestBrands = () => {
   return (
@@ -12,10 +10,14 @@ const BestBrands = () => {
         </h2>
 
         <div className="flex flex-1 flex-wrap justify-evenly items-center md:flex-row flex-col py-8 gap-20">
-            <img src={Nike} alt="nike logo" className="md:w-[15%] w-[150px] hover:scale-125 transition-transform duration-500 transform origin-center"  />
-            <img src={Jordan} alt="jordan logo"  className="md:w-[15%] w-[150px] hover:scale-125 transition-transform duration-500 transform origin-center" />
-            <img src={OffWhite} alt="offwhite logo" className="md:w-[15%] w-[150px] hover:scale-125 transition-transform duration-500 transform origin-center" />
-            <img src={Yeezy} alt="yeezy" className="md:w-[15%] w-[150px] hover:scale-125 transition-transform duration-500 transform origin-center" />
+            {BrandImages.map((brand)=> (
+              <img 
+                key={brand.alt}
+                src={brand.src}
+                alt={brand.alt}
+                className="md:w-[15%] w-[150px] hover:scale-125 transition-transform duration-500 transform origin-center"
+              />
+            ))}
         </div>
       </div>
     </section>
