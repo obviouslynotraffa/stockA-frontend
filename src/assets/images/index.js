b709ba32c63d925d71a2483136f71b90c9534bf4
raffaele <raffaele.fontana.02@gmail.com>
2024-04-03 22:42:52 +0200

    add sneakers x trevis collab images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,11 +3,19 @@ import Jordan from './jordan-logo.png'
 import Nike from './nike-logo.png'
 import OffWhite from './offwhite-logo.png'
 import Yeezy from './yeezy-logo.png'
+import JordanTrevis from './trevis.png'
+import DunkTrevis from './dunktrevis.jpg'
+import AirForceTrevis from './lowtravis.jpg'
+import ReactTrevis from './reacttrevis.jpg'
 
 export {
     HeroImg,
     Jordan,
     Nike,
     OffWhite,
-    Yeezy
+    Yeezy,
+    JordanTrevis,
+    DunkTrevis,
+    AirForceTrevis,
+    ReactTrevis,
 }
