2b8c440b4d78142a87b1d7ae249223e307800140
raffaele <raffaele.fontana.02@gmail.com>
2024-04-09 18:00:15 +0200

    add travis scott sneakers images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -3,10 +3,10 @@ import Jordan from './jordan-logo.png'
 import Nike from './nike-logo.png'
 import OffWhite from './offwhite-logo.png'
 import Yeezy from './yeezy-logo.png'
-import JordanTrevis from './trevis.png'
-import DunkTrevis from './dunktravis.jpg'
-import AirForceTrevis from './lowtravis.jpg'
-import ReactTrevis from './reacttrevis.jpg'
+import JordanTrevis from './travis-jordan.png'
+import DunkTrevis from './travis-dunklow.png'
+import AirForceTrevis from './travis-airforce.png'
+import ReactTrevis from './travis-react.png'
 
 export {
     HeroImg,
@@ -17,5 +17,5 @@ export {
     JordanTrevis,
     DunkTrevis,
     AirForceTrevis,
-    ReactTrevis,
+    ReactTrevis
 }
