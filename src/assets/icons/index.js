1c4ef43a50f05a2e443c38b268e023b24e38fd4b
raffaele <raffaele.fontana.02@gmail.com>
2024-04-10 19:51:59 +0200

    add services icon to export

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,10 +2,16 @@ import blackLogo from './blackLogo.png'
 import whiteLogo from './whiteLogo.png'
 import tabLogo from './tabLogo.png'
 import hamburger from './hamburger.svg'
+import serviceIcon1 from './support.svg'
+import serviceIcon2 from './truck-fast.svg'
+import serviceIcon3 from './shield-tick.svg'
 
 export{
     blackLogo,
     whiteLogo,
     tabLogo,
-    hamburger
+    hamburger,
+    serviceIcon1,
+    serviceIcon2,
+    serviceIcon3
 }
