37b75559061f73710014acf519f14c7f8abb46ac
raffaele <raffaele.fontana.02@gmail.com>
2024-04-10 19:52:41 +0200

    add services section

---
+++ src/components/Home/Services.jsx
@@ -0,0 +1,21 @@
+import { services } from "../../constants"
+
+const Services = () => {
+  return (
+    <section className='padding-x padding-y max-container'>
+      <div className='w-full max-w-screen-wide m-auto flex justify-center wrap gap-9'>
+        {services.map((service)=> (
+           <div className="flex-1 sm:w-[350px sm:min-w-[350] w-full rounded-[20px] shadow-3xl px-10 py-16">
+            <div className="w-11 h-11 flex justify-center items-center bg-flag-color rounded-full">
+                <img src={service.imgURL} alt={service.label} className="w-[24px] h-[24px]" />
+            </div>
+            <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold">{service.label}</h3>
+            <p className="mt-3 break-words font-montserrat text-xl leading-normal text-slate-gray">{service.subtext}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default Services
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/Home/Hero"
 import BestBrands from "../components/Home/BestBrands"
 import TravisScottSection from "../components/Home/TravisScottSection"
 import Subscribe from "../components/Home/Subscribe"
+import Services from "../components/Home/Services"
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <BestBrands />
       <TravisScottSection/>
       <Subscribe/>
+      <Services/>
     </section>
   )
 }
