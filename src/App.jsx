6bf059e6913fde2133a99d116be0368b9b95ed19
raffaele <raffaele.fontana.02@gmail.com>
2024-04-12 15:11:49 +0200

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,21 @@ import Home from './pages/Home';
 import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from './pages/Contact';
+import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <main className='relative'>
       <Router>
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />}/>
           <Route path="/products" exact element={<Products />}/> 
           <Route path="/about" exact element={<About />}/> 
           <Route path="/contact" exact element={<Contact />}/> 
         </Routes>
+        <Footer />
       </Router>
     </main>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,80 @@
+import { whiteLogo } from "../assets/icons"
+import { socials, appStores } from "../constants"
+import { footerLinks } from "../constants"
+
+const Footer = () => {
+  return (
+    <section className="w-full py-8 max-container bg-black">
+        <div className="padding-x text-white max-w-screen-wide m-auto flex flex-col pt-3">
+            <div className="flex flex-row flex-wrap gap-5 justify-around items-start pb-8 ">
+                <div className="flex flex-col items-start gap-5 w-[275px] ">
+                    <img 
+                        src={whiteLogo}
+                        alt="StockA logo"
+                        width={150}
+                        height={150} 
+                    />
+                    <p className="text-zinc-400 font-montserrat font-medium">
+                        Passion, style, and comfort come together in every step. Explore our range of sneakers to meet your every fashion and functionality desire.
+                    </p>
+                    <div className='flex items-center gap-5 mt-2'>
+                        {socials.map((social) => (
+                            <div className="flex items-center justify-center w-12 h-12 bg-white rounded-full gap-8 hover:cursor-pointer hover:bg-slate-gray">
+                                <img 
+                                    src={social.src}
+                                    alt={social.alt}
+                                    width={24}
+                                    height={24} 
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                
+                {footerLinks.map((section)=> (
+                    <div className="w-[170px]">
+                        <h4 className="font-bold text-2xl py-5">
+                            {section.title}
+                        </h4>
+                        <ul>
+                            {section.links.map((link) => (
+                                <li className="text-zinc-400 font-medium font-montserrat py-1 hover:underline hover:text-slate-gray">
+                                    <a href={link.link}>
+                                    {link.name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>   
+                ))}
+
+                <div className="flex flex-col gap-10 w-[295px]">
+                    <p className="font-bold font-palanquin text-2xl text-white max-sm:pt-5">
+                        Download our App!
+                    </p>
+                    <p className="font-montserrat text-zinc-400  font-medium">
+                        Download the App from Play Store and Apple Store to have a better experience.
+                    </p>
+                    <div className="flex justify-center items-center gap-3 flex-wrap">
+                        {appStores.map((store) => (
+                            <img 
+                                alt={store.alt}
+                                src={store.src}
+                                width={140}
+                                height={140}
+                                className="hover:cursor-pointer"
+                            />
+                        ))}
+                    </div> 
+                </div>
+            </div>
+
+            <div className="pt-8 border-t-2 border-white-400 text-center text-zinc-400 font-montserrat font-bold">
+                Copyright 2024 - StockA
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Footer
