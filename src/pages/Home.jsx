774503f721d7b56e6d9767b146f5ae2d36572770
raffaele <raffaele.fontana.02@gmail.com>
2024-04-13 17:37:09 +0200

    move sections to its folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import Hero from "../components/Home/Hero"
-import BestBrands from "../components/Home/BestBrands"
-import TravisScottSection from "../components/Home/TravisScottSection"
-import Subscribe from "../components/Home/Subscribe"
-import Services from "../components/Home/Services"
-import CustomerReviews from "../components/Home/CustomerReviews"
+import Hero from "../sections/Home/Hero"
+import BestBrands from "../sections/Home/BestBrands"
+import TravisScottSection from "../sections/Home/TravisScottSection"
+import Subscribe from "../sections/Home/Subscribe"
+import Services from "../sections/Home/Services"
+import CustomerReviews from "../sections/Home/CustomerReviews"
 
 const Home = () => {
   return (
