4fdee0d56bdef49bb6fbafd097b2d0abef4ab846
raffaele <raffaele.fontana.02@gmail.com>
2024-04-30 00:02:13 +0200

    Add CardContainer component to Products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,11 @@
 import Banner from "../sections/Products/Banner"
+import CardContainer from "../sections/Products/CardContainer"
 
 const Products = () => {
   return (
     <section>
       <Banner />
+      <CardContainer />
     </section>
   )
 }
