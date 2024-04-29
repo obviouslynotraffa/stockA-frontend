d4f1d3e309d570cc839578b0d2f5d6232c3fe898
raffaele <raffaele.fontana.02@gmail.com>
2024-04-30 00:01:10 +0200

    export products info

---
+++ src/constants/products.js
@@ -0,0 +1,93 @@
+
+import { 
+    courtPurpleWhite,
+    fearlessOg,
+    atmosphere, 
+    bredPatent,
+    electroOrange,
+    luckyGreen,
+    obsidian,
+    skyline,
+    spiderMan,
+    stealth,
+    uncToe,
+    visionaire,
+    yellowToe,
+    shatteredBackboard 
+        } from "../assets/images";
+
+
+
+
+export const PRODUCTS = [
+    {
+        imgUrl: courtPurpleWhite,
+        shoeName: "Air Jordan 1 Retro High OG Court Purple White",
+        price: 259
+    },
+    {
+        imgUrl: fearlessOg,
+        shoeName: "Air Jordan 1 Retro High Fearless OG",
+        price: 519
+    },
+    {
+        imgUrl: atmosphere,
+        shoeName: "Air Jordan 1 Retro High OG Atmosphere",
+        price: 350
+    },
+    {
+        imgUrl: bredPatent,
+        shoeName: "Air Jordan 1 Retro High OG Bred Patent",
+        price: 419
+    },
+    {
+        imgUrl: electroOrange,
+        shoeName: "Air Jordan 1 Retro High OG Electro Orange",
+        price: 205
+    },
+    {
+        imgUrl: luckyGreen,
+        shoeName: "Air Jordan 1 Retro High OG Lucky Green",
+        price: 349
+    },
+    {
+        imgUrl: obsidian,
+        shoeName: "Air Jordan 1 Retro High OG Obsidian UNC 2019",
+        price: 510
+    },
+    {
+        imgUrl: skyline,
+        shoeName: "Air Jordan 1 Retro High OG Skyline",
+        price: 538
+    },
+    {
+        imgUrl: spiderMan,
+        shoeName: "Air Jordan 1 Retro High OG Spider Man Across The Spider Verse",
+        price: 299
+    },
+    {
+        imgUrl: stealth,
+        shoeName: "Air Jordan 1 Retro High OG Stealth",
+        price: 200
+    },
+    {
+        imgUrl: uncToe,
+        shoeName: "Air Jordan 1 Retro High OG UNC Toe",
+        price: 319
+    },
+    {
+        imgUrl: visionaire,
+        shoeName: "Air Jordan 1 Retro High OG Visionaire",
+        price: 299
+    },
+    {
+        imgUrl: yellowToe,
+        shoeName: "Air Jordan 1 Retro High OG Yellow Toe",
+        price: 269
+    },
+    {
+        imgUrl: shatteredBackboard,
+        shoeName: "Air Jordan 1 Retro High Shattered Backboard 3.0",
+        price: 409
+    }
+];
