122d21a271ca3d2538dc5411c74f088ae6e1f045
raffaele <raffaele.fontana.02@gmail.com>
2024-04-30 15:10:56 +0200

    Update banner in Products page

--- src/sections/Products/Banner.jsx
+++ src/sections/Products/Banner.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import sneaker_sound from '../../assets/sneaker_sound.mp3'
 
 const Banner = () => {
+
+  function play() {
+    new Audio(sneaker_sound).play()
+  }
+
+
   return (
-    <section className='padding-x padding-y max-container'>
-        <div className='w-full max-w-screen-wide m-auto bg-gray-400 py-8 px-6 flex flex-col gap-3 rounded-xl'>
+    <section className='w-full py-10 padding-x max-w-screen-wide max-container'>
+        <div className='bg-gray-300 py-8 px-8 md:px-24 flex flex-col gap-3 rounded-xl'>
             <div className='font-bold font-palanquin text-4xl'>
               sneaker
             </div>
@@ -11,11 +17,43 @@ const Banner = () => {
               <p>
                 noun
               </p>
-              <p>
-              /ˈsniː.kər/ or /ˈsniː.kɚ/
-              </p>
+              <div className='flex fle-row gap-4'>
+                <p className='font-bold'>
+                  /ˈsniː.kɚ/
+                </p>
+                <button
+                  onClick={play}
+                  className='w-[30px] h-[30px] bg-sound-wave'
+           
+                >
+                  
+                  <svg 
+                    width="32px" 
+                    height="32px" 
+                    viewBox="0 0 24 24" 
+                    fill="none" 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    aria-labelledby="soundsIconTitle" 
+                    stroke="#000000" 
+                    stroke-width="2" 
+                    stroke-linecap="round" 
+                    stroke-linejoin="round" 
+                    color="#000000"
+                  > 
+                    <title id="soundsIconTitle">Listen to the American English pronunciation</title> 
+                    <path d="M12 7L12 17"/> 
+                    <path d="M15 10L15 14"/> 
+                    <path d="M18 8L18 16"/> 
+                    <path d="M21 13L21 11"/> 
+                    <path d="M9 4L9 20"/> 
+                    <path d="M6 9L6 15"/> 
+                    <path d="M3 13L3 11"/> 
+                  </svg>
+                </button>
+              </div>
+              
             </div>
-            <p className='font-palanquin text-lg'>
+            <p className='font-palanquin text-lg leading-6'>
             is a lightweight and comfortable sports shoe, typically characterized by a rubber sole and a fabric or synthetic upper. It is primarily designed for activities such as walking, running, or exercising, but is often worn for fashion or casual purposes as well.
             </p>
         </div>
