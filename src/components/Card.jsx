5fbc28970d5883358314bc7d06476faec1bf0891
raffaele <raffaele.fontana.02@gmail.com>
2024-04-30 15:06:00 +0200

    Update card width

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,13 @@
 
 const Card = ({imgURL, shoeName, price}) => {
   return (
-    <div className='w-[200px] h-[250px] flex flex-col py-4 px-4 hover:underline hover:cursor-pointer'>
+    <div className='w-[160px] h-[200px] flex flex-col py-4 px-4 hover:underline hover:cursor-pointer'>
         <img 
             src={imgURL}
             alt={shoeName}
             className='object-contain'
         />
-        <div className='w-full font-montserrat leading-5 '>
+        <div className='w-full font-montserrat leading-5 line-clamp-3'>
             {shoeName}
         </div>
         <div className='font-bold text-2xl font-montserrat mt-auto'>
