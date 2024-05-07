1a6a83b2c8a37b6d6b9d133cd582444717a03ef5
raffaele <raffaele.fontana.02@gmail.com>
2024-05-08 00:24:37 +0200

    update responsive

--- src/sections/Products/CardContainer.jsx
+++ src/sections/Products/CardContainer.jsx
@@ -3,7 +3,7 @@ import { PRODUCTS } from "../../constants/products"
 
 const CardContainer = () => {
   return (
-    <section className='py-16 max-container w-full max-w-screen-wide m-auto padding-x'>
+    <section className='py-16 max-container w-full max-w-screen-wide m-auto padding-x max-sm:px-0 '>
         <div className=''>
             <div className='flex flex-row gap-3 justify-center items-center flex-wrap  '>
                 {PRODUCTS.map((product) => (
