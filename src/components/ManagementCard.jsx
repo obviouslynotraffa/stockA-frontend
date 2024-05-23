740b486543a69e1e99f9e456f2a02a2a1f27036c
raffaele <raffaele.fontana.02@gmail.com>
2024-05-23 22:16:28 +0200

    fix responsive management card

--- src/components/ManagementCard.jsx
+++ src/components/ManagementCard.jsx
@@ -2,17 +2,17 @@
 
 const ManagementCard = ({imgURL, name, title}) => {
   return (
-    <div className='flex flex-col max-w-[250px] group hover:cursor-pointer'>
+    <div className='flex flex-col max-sm:max-w-[40%] max-md:max-w-[200px] max-lg:max-w-[25%] max-w-[20%] group hover:cursor-pointer'>
                 <img 
                     src={imgURL} 
                     alt={name}
                     width={300}
                     height={300}
                 />
-                <p className='text-2xl font-montserrat group-hover:text-flag-color group-hover:underline pt-3 pb-1'>
+                <p className='xl:text-2xl text-xl font-montserrat group-hover:text-flag-color group-hover:underline pt-3 pb-1'>
                     {name}
                 </p>
-                <p className='font-palanquin text-xl text-gray-600'>
+                <p className='font-palanquin lg:text-lg text-base text-gray-600'>
                     {title}
                 </p>
     </div>
