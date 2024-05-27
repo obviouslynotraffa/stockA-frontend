0b35025e18e3a36eac2b892e3a523a29f1f3dc8a
raffaele <raffaele.fontana.02@gmail.com>
2024-05-27 11:25:55 +0200

    add link to each product card

--- src/sections/Products/CardContainer.jsx
+++ src/sections/Products/CardContainer.jsx
@@ -1,17 +1,21 @@
 import Card from "../../components/Card"
 import { PRODUCTS } from "../../constants/products"
+import { Link } from 'react-router-dom';
+
 
 const CardContainer = () => {
   return (
     <section className='py-16 max-container w-full max-w-screen-wide m-auto padding-x max-sm:px-0 '>
             <div className='flex flex-row gap-3 justify-center items-center flex-wrap  '>
                 {PRODUCTS.map((product) => (
+                  <Link to={`/product/${product.URLName}`} key={product.shoeName} >
                     <Card
                         key={product.shoeName}
                         imgURL={product.imgUrl}
                         shoeName={product.shoeName}
                         price={product.price}
                     />
+                  </Link>
                 ))}
     
             </div>
