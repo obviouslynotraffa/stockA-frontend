b1d7d5b756cdb7695fa38eedc2c6cd3f322c9ee3
raffaele <raffaele.fontana.02@gmail.com>
2024-05-28 11:41:38 +0200

    update product file

--- src/constants/products.js
+++ src/constants/products.js
@@ -30,120 +30,180 @@ export const PRODUCTS = [
         imgUrl: courtPurpleWhite,
         shoeName: "Air Jordan 1 Retro High OG Court Purple White",
         price: 259,
-        URLName: "air-jordan-1-retro-high-og-court-purple-white"
+        URLName: "air-jordan-1-retro-high-og-court-purple-white",
+        SKUD: "SKU: 555088-501",
+        desc: "The Air Jordan 1 High Court Purple White is a colorway that was first released in 2018. The shoe features a white leather upper with purple overlays. The outsole is made out of rubber and features a purple outsole. The shoe is finished off with a white midsole. ",
+        date: "04/2018"
     },
     {
         imgUrl: fearlessOg,
         shoeName: "Air Jordan 1 Retro High Fearless OG",
         price: 519,
-        URLName: "air-jordan-1-retro-high-fearless-og"
+        URLName: "air-jordan-1-retro-high-fearless-og",
+        SKUD: "SKU: BV0006-900",
+        desc: "The Air Jordan 1 High Fearless OG is a colorway that was first released in 2019. The shoe features a white leather upper with red overlays. The outsole is made out of rubber and features a red outsole. The shoe is finished off with a white midsole. ",
+        date: "11/2019"
     },
     {
         imgUrl: atmosphere,
         shoeName: "Air Jordan 1 Retro High OG Atmosphere",
         price: 349,
-        URLName: "air-jordan-1-retro-high-og-atmosphere"
+        URLName: "air-jordan-1-retro-high-og-atmosphere",
+        SKUD: "SKU: DD9335-641",
+        desc: "The Air Jordan 1 High Atmosphere pays a tribute to the gum Michael Jordan was chewing during his career. The outsole features a red spot, symbolizing the gum. The pink panels are made out of patent leather, while the dark blue sides are made out of soft leather. ",
+        date: "12/2018"
     },
     {
         imgUrl: bredPatent,
         shoeName: "Air Jordan 1 Retro High OG Bred Patent",
         price: 419,
-        URLName: "air-jordan-1-retro-high-og-bred-patent"
+        URLName: "air-jordan-1-retro-high-og-bred-patent",
+        SKUD: "SKU: 555088-063",
+        desc: "The Air Jordan 1 High Bred Patent is a colorway that was first released in 2019. The shoe features a black leather upper with red overlays. The outsole is made out of rubber and features a red outsole. The shoe is finished off with a white midsole. ",
+        date: "12/2019"
     },
     {
         imgUrl: electroOrange,
         shoeName: "Air Jordan 1 Retro High OG Electro Orange",
         price: 209,
-        URLName: "air-jordan-1-retro-high-og-electro-orange"
+        URLName: "air-jordan-1-retro-high-og-electro-orange",
+        SKUD: "SKU: 555088-180",
+        desc: "The Air Jordan 1 High Electro Orange is a colorway that was first released in 2021. The shoe features a white leather upper with orange overlays. The outsole is made out of rubber and features a orange outsole. The shoe is finished off with a white midsole. ",
+        date: "02/2021"
     },
     {
         imgUrl: luckyGreen,
         shoeName: "Air Jordan 1 Retro High OG Lucky Green",
         price: 349,
-        URLName: "air-jordan-1-retro-high-og-lucky-green"
+        URLName: "air-jordan-1-retro-high-og-lucky-green",
+        SKUD: "SKU: 555088-056",
+        desc: "The Air Jordan 1 High Lucky Green is a colorway that was first released in 2020. The shoe features a white leather upper with green overlays. The outsole is made out of rubber and features a green outsole. The shoe is finished off with a white midsole. ",
+        date: "10/2020"
     },
     {
         imgUrl: obsidian,
         shoeName: "Air Jordan 1 Retro High OG Obsidian UNC 2019",
         price: 509,
-        URLName: "air-jordan-1-retro-high-og-obsidian-unc-2019"
+        URLName: "air-jordan-1-retro-high-og-obsidian-unc-2019",
+        SKUD: "SKU: 555088-140",
+        desc: "The Air Jordan 1 High Obsidian UNC 2019 is a colorway that was first released in 2019. The shoe features a white leather upper with blue overlays. The outsole is made out of rubber and features a blue outsole. The shoe is finished off with a white midsole.",
+        date: "08/2019"
     },
     {
         imgUrl: skyline,
         shoeName: "Air Jordan 1 Retro High OG Skyline",
         price: 539,
-        URLName: "air-jordan-1-retro-high-og-skyline"
+        URLName: "air-jordan-1-retro-high-og-skyline",
+        SKUD: "SKU: 555088-015",
+        desc: "The Air Jordan 1 High Skyline is a colorway that was first released in 2019. The shoe features a white leather upper with black overlays. The outsole is made out of rubber and features a black outsole. The shoe is finished off with a white midsole. ",
+        date: "05/2019"
     },
     {
         imgUrl: spiderMan,
         shoeName: "Air Jordan 1 Retro High OG Spider Man Across The Spider Verse",
         price: 299,
-        URLName: "air-jordan-1-retro-high-og-spider-man-across-the-spider-verse"
+        URLName: "air-jordan-1-retro-high-og-spider-man-across-the-spider-verse",
+        SKUD: "SKU: 555088-602",
+        desc: "The Air Jordan 1 Retro High OG Spider Man Across The Spider Verse is a vibrant and unique colorway inspired by the iconic superhero. First released in 2023, this shoe pays homage to the Spider-Man universe with its distinctive design. The upper is constructed with a mix of red, blue, and black overlays, mimicking Spider-Man's suit. The sneaker features a translucent red outsole, symbolizing Spider-Man's agility and web-slinging action. The shoe is completed with a white midsole and subtle web patterns throughout the design, adding to its thematic appeal. This special edition is a must-have for both sneaker enthusiasts and Spider-Man fans alike.",
+        date: "07/2023"
     },
     {
         imgUrl: stealth,
         shoeName: "Air Jordan 1 Retro High OG Stealth",
         price: 199,
-        URLName: "air-jordan-1-retro-high-og-stealth"
+        URLName: "air-jordan-1-retro-high-og-stealth",
+        SKUD: "SKU: 555088-035",
+        desc: "The Air Jordan 1 High Stealth is a colorway that was first released in 2021. The shoe features a white leather upper with black overlays. The outsole is made out of rubber and features a black outsole. The shoe is finished off with a white midsole. ",
+        date: "07/2021"
     },
     {
         imgUrl: uncToe,
         shoeName: "Air Jordan 1 Retro High OG UNC Toe",
         price: 319,
-        URLName: "air-jordan-1-retro-high-og-unc-toe"
+        URLName: "air-jordan-1-retro-high-og-unc-toe",
+        SKUD: "SKU: 555088-125",
+        desc: "The Air Jordan 1 High UNC Toe is a colorway that was first released in 2020. The shoe features a white leather upper with blue overlays. The outsole is made out of rubber and features a blue outsole. The shoe is finished off with a white midsole. ",
+        date: "02/2020"
     },
     {
         imgUrl: visionaire,
         shoeName: "Air Jordan 1 Retro High OG Visionaire",
         price: 299,
-        URLName: "air-jordan-1-retro-high-og-visionaire"
+        URLName: "air-jordan-1-retro-high-og-visionaire",
+        SKUD: "SKU: 555088-001",
+        desc: "The Air Jordan 1 High Visionaire is a colorway that was first released in 2021. The shoe features a white leather upper with black overlays. The outsole is made out of rubber and features a black outsole. The shoe is finished off with a white midsole. ",
+        date: "06/2021"
     },
     {
         imgUrl: yellowToe,
         shoeName: "Air Jordan 1 Retro High OG Yellow Toe",
         price: 269,
-        URLName: "air-jordan-1-retro-high-og-yellow-toe"
+        URLName: "air-jordan-1-retro-high-og-yellow-toe",
+        SKUD: "SKU: 555088-109",
+        desc: "The Air Jordan 1 High Yellow Toe is a colorway that was first released in 2020. The shoe features a white leather upper with yellow overlays. The outsole is made out of rubber and features a yellow outsole. The shoe is finished off with a white midsole. ",
+        date: "08/2020"
     },
     {
         imgUrl: shatteredBackboard,
         shoeName: "Air Jordan 1 Retro High Shattered Backboard 3.0",
         price: 409,
-        URLName: "air-jordan-1-retro-high-shattered-backboard-3-0"
+        URLName: "air-jordan-1-retro-high-shattered-backboard-3-0",
+        SKUD: "SKU: 555088-028",
+        desc: "The Air Jordan 1 High Shattered Backboard 3.0 is a colorway that was first released in 2019. The shoe features a white leather upper with orange overlays. The outsole is made out of rubber and features a orange outsole. The shoe is finished off with a white midsole. ",
+        date: "10/2019"
     },
     {
         imgUrl: elephant,
         shoeName: "Air Jordan 1 Retro High OG Cement",
         price: 344,
-        URLName: "air-jordan-1-retro-high-og-cement"
+        URLName: "air-jordan-1-retro-high-og-cement",
+        SKUD: "SKU: 555088-048",
+        desc: "The Air Jordan 1 High Cement is a colorway that was first released in 2021. The shoe features a white leather upper with black overlays. The outsole is made out of rubber and features a black outsole. The shoe is finished off with a white midsole. ",
+        date: "05/2021"
     },
     {
         imgUrl: lightFusion,
         shoeName: "Air Jordan 1 Retro High Light Fusion Red",
         price: 299,
-        URLName: "air-jordan-1-retro-high-light-fusion-red"
+        URLName: "air-jordan-1-retro-high-light-fusion-red",
+        SKUD: "SKU: 555088-603",
+        desc: "The Air Jordan 1 High Light Fusion Red is a colorway that was first released in 2021. The shoe features a white leather upper with red overlays. The outsole is made out of rubber and features a red outsole. The shoe is finished off with a white midsole. ",
+        date: "06/2021"
     },
     {
         imgUrl: handCrafted,
         shoeName: "Air Jordan 1 Retro High OG Craft",
         price: 429,
-        URLName: "air-jordan-1-retro-high-og-craft"
+        URLName: "air-jordan-1-retro-high-og-craft",
+        SKUD: "SKU: 555088-201",
+        desc: "The Air Jordan 1 High Craft is a colorway that was first released in 2021. The shoe features a white leather upper with brown overlays. The outsole is made out of rubber and features a brown outsole. The shoe is finished off with a white midsole. ",
+        date: "08/2021"
     },
     {
         imgUrl: silverToe,
         shoeName: "Air Jordan 1 Retro High Silver Toe",
         price: 269,
-        URLName: "air-jordan-1-retro-high-silver-toe"
+        URLName: "air-jordan-1-retro-high-silver-toe",
+        SKUD: "SKU: 555088-105",
+        desc: "The Air Jordan 1 High Silver Toe is a colorway that was first released in 2021. The shoe features a white leather upper with silver overlays. The outsole is made out of rubber and features a silver outsole. The shoe is finished off with a white midsole. ",
+        date: "02/2021"
     },
     {
         imgUrl: royalToe,
         shoeName: "Air Jordan 1 Retro High Royal Toe",
         price: 229,
-        URLName: "air-jordan-1-retro-high-royal-toe"
+        URLName: "air-jordan-1-retro-high-royal-toe",
+        SKUD: "SKU: 555088-041",
+        desc: "The Air Jordan 1 High Royal Toe is a colorway that was first released in 2020. The shoe features a white leather upper with blue overlays. The outsole is made out of rubber and features a blue outsole. The shoe is finished off with a white midsole. ",
+        date: "05/2020"
     },
     {
         imgUrl: universityBlue,
         shoeName: "Air Jordan 1 Retro High University Blue",
         price: 349,
-        URLName: "air-jordan-1-retro-high-university-blue"
+        URLName: "air-jordan-1-retro-high-university-blue",
+        SKUD: "SKU: 555088-134",
+        desc: "The Air Jordan 1 High University Blue is a colorway that was first released in 2021. The shoe features a white leather upper with blue overlays. The outsole is made out of rubber and features a blue outsole. The shoe is finished off with a white midsole.",
+        date: "02/2021"
     }
 ];
