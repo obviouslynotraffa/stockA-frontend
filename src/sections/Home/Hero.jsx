df66c90a8f01e143c0d0ee0a11e7a82a35d7a318
raffaele <raffaele.fontana.02@gmail.com>
2024-06-02 20:27:47 +0200

    update hero section

--- src/sections/Home/Hero.jsx
+++ src/sections/Home/Hero.jsx
@@ -5,39 +5,39 @@ const Hero = () => {
     return (
         <section className='bg-gradient-to-b from-white to-gray-300'>
           <div className='padding-x pb-8 w-full max-w-screen-wide m-auto'>
-            <div className='flex lg:flex-row flex-col justify-evenly items-center md:px-5 pb-10 gap-5 lg:gap-0 '>
-              <div className='flex py-5 flex-col justify-center lg:w-[50%] w-[90%] pb-20 md:pb-0'>
+            <div className='flex flex-col md:flex-row lg:justify-around justify-center items-center md:px-5 pb-10 gap-5'>
+              <div className='flex py-5 flex-col justify-around lg:w-[50%] w-[90%] pb-20 md:pb-0'>
 
-                <p className='text-slate-gray text-2xl leading-6'>
-                  Don't miss the most recent release!
-                </p>
+                <div className='flex flex-col justify-center'>
+                  <p className='text-slate-gray text-2xl leading-6'>
+                    Don't miss the most recent release!
+                  </p>
 
-                <h1 className='xl:text-8xl md:text-[4rem] md:leading-20 text-4xl font-bold font-montserrat py-6'>
-                  Give Your Outfit A <span className='text-flag-color'>New</span> Style
-                </h1>
+                  <h1 className='xl:text-8xl md:text-[4rem] md:leading-20 text-4xl font-bold font-montserrat py-6'>
+                    Give Your Outfit A <span className='text-flag-color'>New</span> Style
+                  </h1>
 
-                <p className='text-slate-gray text-2xl leading-6'>
-                  Buy limited editions sneakers.
-                </p>
+                  <p className='text-slate-gray text-2xl leading-6'>
+                    Buy limited editions sneakers.
+                  </p>
+                </div>
+                
 
-                <div className='flex justify-center items-center'>
+                <div className='flex justify-center items-center md:mt-5'>
                   <Link 
                     to="/products" 
-                    className='bg-flag-color mt-5 rounded-full py-2.5 w-[200px] hover:bg-green-900 flex justify-center items-center'
+                    className='bg-flag-color mt-5 rounded-full py-2.5 w-[200px] hover:bg-green-900 flex justify-center items-center md:mt-5'
                   >
-                    <span className='text-white font-semibold hover:underline'>
+                    <button className='text-white font-semibold hover:underline '>
                       Explore Now
-                    </span>
+                    </button>
                   </Link>
                 </div>
                 
               </div>
-
-              <img 
-                src={HeroImg} 
-                alt=""
-                className='w-[70%] lg:w-[35%] object-cover md:object-contain rounded-lg md:shadow-2xl'
-              />
+      
+              <div className="w-[200px] h-[200px] md:w-[300px] md:h-[500px] bg-hero bg-contain bg-center bg-no-repeat">
+              </div> 
 
             </div>
           </div>
